8b3a05283cdfe1bc4c8b03993e73625198529f4a
Evan Dagur <176645652+prakharevan1@users.noreply.github.com>
2025-12-24 17:46:44 -0800

    Atbash encryption cipher

--- Cipher.java
+++ Cipher.java
@@ -1,4 +1,25 @@
 public class Cipher {
     public static void main(String[] args) {
+        System.out.println(atBash("HELLOWORLD"));
+    }
+
+    public static String atBash(String message) {
+        String encryptedMessage = "";
+        for (int i = 0; i < message.length(); i++) {
+            // get char
+            char currentCharacter = message.charAt(i);
+            // convert the character to a range of 0-25 corresponding to A-Z
+            int encryptedChar = (int) (currentCharacter - 65);
+
+            // map it to its opposite letter on the alphabet spectrum (A -> Z, B -> Y, ...)
+            encryptedChar = 25 - encryptedChar;
+
+            // convert back to ASCII
+            encryptedChar += 'A';
+
+            // append to encrypted message
+            encryptedMessage += (char) encryptedChar;
+        }
+        return encryptedMessage;
     }
 }
