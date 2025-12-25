4a006f53a21fc7a1973c8b0e8ddcd2919a063735
Evan Dagur <176645652+prakharevan1@users.noreply.github.com>
2025-12-24 17:47:42 -0800

    special characters to atbash encryption

--- Cipher.java
+++ Cipher.java
@@ -1,6 +1,8 @@
 public class Cipher {
     public static void main(String[] args) {
-        System.out.println(atBash("HELLOWORLD"));
+        String message = "You are an idiot. :)";
+        System.out.println(message);
+        System.out.println(atBash(message));
     }
 
     public static String atBash(String message) {
@@ -8,18 +10,40 @@ public class Cipher {
         for (int i = 0; i < message.length(); i++) {
             // get char
             char currentCharacter = message.charAt(i);
+
+            // check for special characters
+            if (isSpecialCharacter(currentCharacter)) {
+                encryptedMessage += message.charAt(i);
+                continue;
+            }
+
+            // check for lowercase characters
+            boolean lowerCase = false;
+            if (currentCharacter > 90) {
+                lowerCase = true;
+                currentCharacter -= 32;
+            }
             // convert the character to a range of 0-25 corresponding to A-Z
-            int encryptedChar = (int) (currentCharacter - 65);
+            int encryptedCharNum = (int) (currentCharacter - 65);
 
             // map it to its opposite letter on the alphabet spectrum (A -> Z, B -> Y, ...)
-            encryptedChar = 25 - encryptedChar;
+            encryptedCharNum = 25 - encryptedCharNum;
 
             // convert back to ASCII
-            encryptedChar += 'A';
+            encryptedCharNum += 'A';
+            char encryptedCharacter = (char) encryptedCharNum;
 
-            // append to encrypted message
-            encryptedMessage += (char) encryptedChar;
+            // check for uppercase or lowercase
+            if (lowerCase) {
+                encryptedMessage += String.valueOf(encryptedCharacter).toLowerCase();
+                continue;
+            }
+            encryptedMessage += String.valueOf(encryptedCharacter).toUpperCase();
         }
         return encryptedMessage;
+    }
+
+    public static boolean isSpecialCharacter(char character) {
+        return character < 65 || character > 122;
     }
 }
